3ddfebef88312212f0e079cf47ad40d650b9eef0
Payton <plboekhout@gmail.com>
2020-02-25 11:42:03 -0600

    Assignment 5

--- assignments/assignment-five/05-testing/assign5_test.py
+++ assignments/assignment-five/05-testing/assign5_test.py
@@ -1,3 +1,7 @@
+#Payton Boekhout
+#plb4dq
+#PyTest Assignment 5
+
 import json
 import pytest
 import Professor
@@ -8,11 +12,10 @@ import random
 import string
 
 student = 'akend3'
+course = 'databases'
 assignment = 'assignment1'
 user = 'goggins'
 password = 'augurrox'
-course = 'databases'
-
 
 def test_login(grading_system):
     users = grading_system.users
@@ -31,7 +34,6 @@ def test_password(grading_system):
               assert True
 
 
-
 def test_change_grade(grading_system):
     users = grading_system.users
     courses = grading_system.courses
@@ -43,7 +45,6 @@ def test_change_grade(grading_system):
            assert False
        else:
            assert True
-
 
 
 def test_create_assignment(grading_system):
@@ -61,6 +62,7 @@ def test_create_assignment(grading_system):
     else:
         assert False
 
+
 def test_add_Student(grading_system):
     users = grading_system.users
     courses = grading_system.courses
@@ -72,6 +74,7 @@ def test_add_Student(grading_system):
             assert True
         else:
             assert False
+
 
 def test_drop_student(grading_system):
     users = grading_system.users
@@ -85,12 +88,11 @@ def test_drop_student(grading_system):
         assert True
 
 
-
 def test_submit_assignment(grading_system):
     users = grading_system.users
     courses = grading_system.courses
-    stu = Student.Student(student, users, courses)
-    stu.submit_assignment('databases', 'assignment1', 'blah blah blah', '01/21/2021')
+    st = Student.Student(student, users, courses)
+    st.submit_assignment('databases', 'assignment1', 'blah blah blah', '01/21/2021')
 
     for key in users:
         if '01/21/2021' in users['akend3']['courses']['databases']['assignment1']['submission_date']:
@@ -99,18 +101,15 @@ def test_submit_assignment(grading_system):
             assert False
 
 
-
-
 def test_check_ontime(grading_system):
     users = grading_system.users
     courses = grading_system.courses
-    stu = Student.Student(student, users, courses)
+    st = Student.Student(student, users, courses)
 
-    if stu.check_ontime('02/04/2021', '02/03/2021'):
+    if st.check_ontime('02/04/2021', '02/03/2021'):
         assert False
     else:
         assert True
-
 
 
 def test_check_grades(grading_system):
@@ -125,32 +124,22 @@ def test_check_grades(grading_system):
         assert False
 
 
-
 def test_view_assignments(grading_system):
     users = grading_system.users
     courses = grading_system.courses
-    stu = Student.Student('akend3', users, courses)
-    grades = stu.view_assignments('databases')
-    if grades[0][1] == '1/5/20':
+    st = Student.Student('akend3', users, courses)
+    grades = st.view_assignments('databases')
+    if grades[0][1] == '1/14/20':
         assert True
     else:
         assert False
 
 
-#Tests if a TA can change the grades of a student not in their class
-def test_TAGrade(grading_system):
-    users = grading_system.users
-    taperson = TA.TA('cmhbf5',users,course)
-    taperson.change_grade(student,course,assignment,100)
+#These are the tests that I made
 
-    for key in users:
-       if users[student]['courses'][course][assignment]['grade'] == 0:
-           assert False
-       else:
-           assert True
-
-# Tests the ability of a different professor than the one assigned to the class can change class grades
-def test_profAccess(grading_system):
+# This test checks to see if different proffesors can change grades of the
+# classes they dont teach
+def test_proffesor_change(grading_system):
     users = grading_system.users
     courses = grading_system.courses
     prof = Professor.Professor('goggins', users, courses)
@@ -162,42 +151,52 @@ def test_profAccess(grading_system):
         else:
             assert True
 
-def test_ta(grading_system):
+            
+# This test checks to see if a proffesor not teaching a a certain class can
+# create an assignment for it
+def test_wrong_create(grading_system):
+    grading_system.login(user, password)
+    grading_system.usr.create_assignment('assignment5', '02/28/20', 'cloud_computing')
+    grading_system.reload_data()
+    grading_system.login('hdjsr7', 'pass1234')
+    
+    assignments = grading_system.usr.view_assignments('cloud_computing')
+    assert 'assignment5' in assignments
+
+
+# This test checks to see if a proffesor not teaching a class can drop a student
+# from that course
+def test_wrong_drop(grading_system):
+    grading_system.login(user, password)
+    grading_system.usr.drop_student(student, 'comp_sci')
+    grading_system.reload_data()
+    grading_system.login(student, '123454321')
+    
+    courses = grading_system.usr.courses
+    assert 'comp_sci' in courses
+
+
+# This test checks to see if a TA change change the grade of students not
+# in their class
+def test_TA_change(grading_system):
     users = grading_system.users
-    courses = grading_system.courses
-    ta = TA.TA(user, users, courses)
-    ta.create_assignment('assignment4', '01/21/2021', 'databases')
+    person = TA.TA('cmhbf5',users,course)
+    person.change_grade(student,course,assignment,100)
 
-    assignments = []
-    for key in courses:
-            assignments.append([key, courses[key]])
+    for key in users:
+       if users[student]['courses'][course][assignment]['grade'] == 0:
+           assert False
+       else:
+           assert True
 
-    if assignments[1][1]['assignments']['assignment4']:
-        assert False
-    else:
-        assert True
 
-def test_profCheck(grading_system):
-    users = grading_system.users
-    courses = grading_system.courses
-    prof = Professor.Professor(user, users, courses)
-    grades = prof.check_grades(student, course)
+# This test checks to see if a proffesor of a class they dont teach can check
+# those grades
+def test_wrong_check(grading_system):
+    grading_system.login(username, password)
+    grades = grading_system.usr.check_grades(student, 'comp_sci')
+    assert grades == []
 
-    if grades[1][1] == 23:
-        assert True
-    else:
-        assert False
-
-def test_tacheckGrades(grading_system):
-    users = grading_system.users
-    courses = grading_system.courses
-    ta = TA.TA(user, users, courses)
-    grades = ta.check_grades(student, course)
-
-    if grades[1][1] == 23:
-        assert True
-    else:
-        assert False
 
 @pytest.fixture
 def grading_system():
