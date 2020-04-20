27af17d14dc3ae92407c2a8de8d01cd09759f4a0
pboek99 <43300836+pboek99@users.noreply.github.com>
2020-04-20 15:19:17 -0500

    Create login.js

---
+++ assignments/sprint-4/login.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <style>
+        button{
+            font-size:16px;
+        }
+    </style>
+  </head>
+  <body>
+    <h1>Login</h1>
+    <form action="/login" method="post">
+      <div>
+        <label>Username</label>
+        <input>
+      </div>
+      <div>
+        <label>Password</label>
+        <input>
+      </div>
+      <button type="submit">Login</button><br>
+        <a href = "/register">Register</a>
+    </form>
+    
+  </body>
+</html>
